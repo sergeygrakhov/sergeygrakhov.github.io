3e0ee595560f5cda5cca6652463097f9af608423
sergeygrakhov <garrykiev@gmail.com>
2016-03-28 10:33:43 +0300

    js1-2 task 2 fix

--- FE/JS1-2/task2/script/script.js
+++ FE/JS1-2/task2/script/script.js
@@ -10,6 +10,7 @@ userName = prompt('Введите имя пользователя');
 
 for (i = 0; i<arrayNames.length; i++) {
      if (arrayNames[i] == userName) {
-        alert (userName + ", вы успешно вошли");
+            alert (userName + ", вы успешно вошли");
+            break;
     } else if (i==(arrayNames.length-1)) {alert ('Неверное имя пользователя, Вы не вошли')}
 }
