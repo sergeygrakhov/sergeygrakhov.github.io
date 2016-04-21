6aa1b3f1644a3e6df379d4dadc06f0a9ac59cbb7
sergeygrakhov <garrykiev@gmail.com>
2016-04-22 00:05:28 +0300

    js17-18 added css concat and minify

--- FE/JS17-18/gruntfile.js
+++ FE/JS17-18/gruntfile.js
@@ -15,12 +15,30 @@ module.exports = function(grunt) {
                 src: ['js/dist/script.main.min.js'],
                 dest: 'js/dist/script.main.min.js'
             }
+        },
+        concat_css: {
+            all: {
+                src:['css/src/*.css'],
+                dest:'css/dist/styles.min.css'
+            }
+        },
+        cssmin: {
+            target: {
+                files: [{
+                    expand: true,
+                    cwd: 'css/dist',
+                    src: ['*.min.css'],
+                    dest: 'css/dist'
+                }]
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-concat-css');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
 
-    grunt.registerTask('default', ['concat','uglify']);
+    grunt.registerTask('default', ['concat','uglify','concat_css','cssmin']);
 
 };
