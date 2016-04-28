12f5dd77c2fc4c942929cd798a400222cae1c786
sergeygrakhov <garrykiev@gmail.com>
2016-04-28 23:54:16 +0300

    js 19-20   fix added

--- FE/JS19-20/task1/js/script2.js
+++ FE/JS19-20/task1/js/script2.js
@@ -3,6 +3,8 @@ $(function(){
      e.preventDefault();
      var parentClass = $(this).parent().attr('class');
      if (parentClass == 'innactive') {
+         $(this).parent().siblings().removeClass('acctive');
+         $(this).parent().siblings().addClass('innactive');
          $(this).parent().removeClass('innactive');
          $(this).parent().addClass('acctive');
      }
