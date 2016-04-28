ef4ae3fc86062feb5cf50915cb98f14a6b2d7ee4
sergeygrakhov <garrykiev@gmail.com>
2016-04-28 10:37:21 +0300

    js 19-20 more fix added

--- FE/JS9-10/task1/js/script.js
+++ FE/JS9-10/task1/js/script.js
@@ -1,29 +1,7 @@
 (function($) {
     $(function() {
         $('.jcarousel').jcarousel();
-
-        $('.jcarousel-control-prev')
-            .on('jcarouselcontrol:active', function() {
-                $(this).removeClass('inactive');
-            })
-            .on('jcarouselcontrol:inactive', function() {
-                $(this).addClass('inactive');
-            })
-            .jcarouselControl({
-                target: '-=1'
-            });
-
-        $('.jcarousel-control-next')
-            .on('jcarouselcontrol:active', function() {
-                $(this).removeClass('inactive');
-            })
-            .on('jcarouselcontrol:inactive', function() {
-                $(this).addClass('inactive');
-            })
-            .jcarouselControl({
-                target: '+=1'
-            });
-
+        
         $('.jcarousel-pagination')
             .on('jcarouselpagination:active', 'a', function() {
                 $(this).addClass('active');
