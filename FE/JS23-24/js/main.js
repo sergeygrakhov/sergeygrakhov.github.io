e57b09284ffb71b1a9daae3fa4c0ade51212c902
sergeygrakhov <garrykiev@gmail.com>
2016-05-09 13:15:49 +0300

    Homework JS 23-24 added

---
+++ FE/JS23-24/js/main.js
@@ -0,0 +1,21 @@
+requirejs.config({
+    baseUrl: "js",
+    paths: {
+        'jquery': ['https://code.jquery.com/jquery-1.12.3'],
+        'lodash': ['lodash']
+
+    },
+    shim: {
+        'jquery' :{
+            exports: 'jQuery'},
+        'lodash' :{
+            exports: 'lodash'}
+        }
+
+
+});
+require(['model','view','controller','jquery','lodash'], function(view,model,controller) {
+
+
+
+});
---
+++ FE/JS23-24/js/model.js
@@ -0,0 +1,37 @@
+
+define ('model',['jquery'],function(data){
+    function Model(data){
+    var self = this;
+    self.data = data;
+    self.addItem = function (item){
+        if (item.length === 0){
+            return;
+        }
+        self.data.push(item);
+    };
+    self.removeItem = function (item){
+        var index = self.data.indexOf(item);
+        if (index ===-1) {
+            return;
+        }
+        self.data.splice(index, 1);
+        return self.data;
+    };
+    self.editItem = function (item){
+        $('button').attr('disabled',true);
+        var index = self.data.indexOf(item);
+        if (index ===-1) {
+            return;
+        }
+        return [self.data[index],index];
+    };
+    self.saveItem = function (item,index){
+        if (item.length === 0){
+            return;
+        }
+        $('button').removeAttr('disabled');
+        self.data.splice(index,0,item);
+    };
+}
+return new Model(data);
+});
