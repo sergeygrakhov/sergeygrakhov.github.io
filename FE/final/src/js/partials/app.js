9d3f5dc6ca6ff1878487460ab299b7f82ded80b5
sergeygrakhov <garrykiev@gmail.com>
2016-05-16 16:32:45 +0300

    Final work all_versions masonry_plugin - fixed

--- FE/final/src/js/partials/app.js
+++ FE/final/src/js/partials/app.js
@@ -1,5 +1,15 @@
 'use strict';
 $(function(){
+    var $container = $('#container');
+    function initializeMasonry(){
+        $container.imagesLoaded( function() {
+            $container.masonry({
+                columnWidth: function( containerWidth ) {
+                    return containerWidth / 3
+                }
+            });
+        });
+    }
     var pool = ['moto','sport','nature','animals','health','history','relax','cat','flower','auto','computer','robot','science','web','audi','subaru','skoda','bmw'];
     function randomWord (min,max) {
         var random = min - 0.5 + Math.random() * (max-min +1);
@@ -13,22 +23,18 @@ $(function(){
         searchPicture(pool[rand],3);
     }
     function searchPicture(text, quantity) {
+
         var message ='<div class="picture item noPic">There is no pictures for your query</div>';
         if ((text.indexOf('.') !==-1) || (text.indexOf('!') !==-1) || (text.indexOf('?') !==-1)) {
             $('.grid').append(message)}
         else{
         $.getJSON('http://pixabay.com/api/?key=2563157-05cd126e344fd2fa56a52a281&q='+text+'&per_page='+quantity, function (data) {
             if (data.hits.length) {
+
                 var pictureSection = $('#pictureLayout').html();
                 var content = _.template(pictureSection)(data);
                 $('.grid').append(content);
-                var $grid = $('#container').imagesLoaded( function(){
-
-                    $grid.masonry({
-                        itemSelector: 'item',
-                        columnWidth: 250
-                    });
-                });
+                initializeMasonry();
             }
             else $('.grid').append(message);
         });
@@ -41,7 +47,9 @@ $(function(){
         if (string) {
             elem[0].value = '';
             $('.picture').remove();
+            $container.masonry('destroy');
             searchPicture(string, 12);
         }
     });
+
 });
